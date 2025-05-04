2b419fce996b20721773626300aa1ce6e30a5d5e
ishaaaa8 <itsisha4884@gmail.com>
2025-05-04 19:45:39 +0530

    fixed cors error for localhost

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ connectDB();
 // Allowed frontend origins
 const allowedOrigins = [
   "https://medbot-frontend.onrender.com",
-  "https://medbot-ai-5wnt.onrender.com"
+  "https://medbot-ai-5wnt.onrender.com","http://localhost:3000",
 ];
 
 // Configure CORS
